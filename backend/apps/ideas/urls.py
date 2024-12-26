d85687a2a4965312a8c10087f8ca84a0af4cf4c0
Manu21 <emanuel.rusu03@e-uvt.ro>
2024-12-26 13:44:40 +0200

    kkkk

--- backend/apps/ideas/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import IdeaViewSet, TeamViewSet, UserProfileViewSet
-
-router = DefaultRouter()
-router.register('ideas', IdeaViewSet)
-router.register('teams', TeamViewSet)
-router.register('profiles', UserProfileViewSet)
-
-urlpatterns = router.urls
